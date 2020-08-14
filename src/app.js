232ba77e773d9c2476c900948602ba524704c503
keitamu <41756721+keitamu@users.noreply.github.com>
2020-08-15 02:00:22 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,10 @@
 // src/app.js
 const express = require('express')
 const app = express()
-const ShippingController = require('../src/controllers/shipping-controller')
 
-app.get('/*shipping', (request, response) => {
-  let ctrl = new ShippingController()
-
-  ctrl
-    .getItemShipping({id: request.query.itemId, type: request.query.type})
-    .then(amount => {
-      response.send({ itemId: request.query.itemId, priceUSD: amount })
-    })
-    .catch(error => {
-      response.status(500).send({ error: error.message })
-    })
-
+app.get('/products/:productId', (request, response) => {
+      response.send({ weightLB: 50 })
 })
 
-let PORT = process.env.PORT || 3001;
+let PORT = process.env.PORT || 8899;
 app.listen(PORT, () => console.log(`ShippingService is listening on port ${PORT}`))
